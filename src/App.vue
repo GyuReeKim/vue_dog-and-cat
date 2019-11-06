9af006bf68fae2403b077859d0a090803bf96e07
GyuReeKim <starkim96@naver.com>
2019-11-06 17:24:10 +0900

    dog-and-cat

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <GetDogImage/> -->
+    <!-- <GetCatImage/> -->
+
+    <!-- GetImage는 component요소로 자식요소와 연결해준다. -->
+    <!-- btnName은 GetImage의 props 요소 -->
+    <!-- getAnimalImage는 자식 요소에 있고, emit을 사용해 부모요소에서 사용할 수 있다. -->
+    <!-- getImage는 App.vue의 methods -->
+    <GetImage btnName="멍멍" @getAnimalImage="getImage"/>
+    <GetImage btnName="야옹" @getAnimalImage="getImage"/>
+    <!-- animalUrl은 App.vue의 data로 v-bind를 통해 img태그와 연결해준다. -->
+    <img v-bind:src="animalUrl" alt="">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import GetDogImage from './components/GetDogImage.vue'
+// import GetCatImage from './components/GetCatImage.vue'
+import GetImage from './components/GetImage.vue'
+// axios를 사용하므로 import 해줘야 한다.
+import axios from 'axios'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // GetDogImage,
+    // GetCatImage,
+    GetImage
+  },
+  methods: {
+    getImage: function(name){
+      // console.log("이벤트 발생")
+      // console.log(name)
+      if (name === "멍멍") {
+        const DOG_URL = "https://dog.ceo/api/breeds/image/random"
+        axios.get(DOG_URL)
+          .then((response)=>{
+            // data animalUrl에 url정보를 저장해준다.
+            this.animalUrl = response.data.message
+          })
+          .catch((error)=>{
+            console.log(error)
+          })
+      } else {
+        const CAT_URL = "https://api.thecatapi.com/v1/images/search"
+        axios.get(CAT_URL)
+          .then((response)=>{
+            this.animalUrl = response.data[0].url
+          })
+          .catch((error)=>{
+            console.log(error)
+          })
+      }
+    }
+  },
+  data: function(){
+    return {
+      animalUrl: ''
+    }
   }
 }
 </script>
---
+++ src/components/GetImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- click event는 onAnimalImage methods와 연결되어있다. -->
+  <!-- btnName은 props 요소로, 변수처럼 사용 가능하다. -->
+  <button v-on:click="onAnimalImage">{{btnName}}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    btnName: String,
+  },
+  data: function(){
+    return {
+      
+    }
+  },
+  methods: {
+    onAnimalImage: function(){
+      // 이벤트를 올리는 과정
+      console.log(this.btnName)
+      // emit은 자식요소를 부모에서 사용할 수 있도록 해준다.
+      this.$emit('getAnimalImage', this.btnName)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
